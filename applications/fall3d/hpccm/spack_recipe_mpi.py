85255b185bfcc3a7f08f5fea6ab43b8c545f6f1a
Michael Redenti <m.redenti@cineca.it>
2025-01-22 01:40:08 +0100

    fix wrong environment setup at runtime

--- applications/fall3d/hpccm/spack_recipe_mpi.py
+++ applications/fall3d/hpccm/spack_recipe_mpi.py
@@ -286,7 +286,7 @@ Stage1 += copy(
         }, _from='devel')
     
     
-Stage0 += shell(commands=[
+Stage1 += shell(commands=[
     '. /opt/spack/share/spack/setup-env.sh', 
     'spack env activate /opt/spack-environment',
     # Export path to Fall3D
