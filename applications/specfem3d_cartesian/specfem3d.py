8c277249621799523662c066c97bc6869f8e8bb2
Michael Redenti <mredenti@login02.leonardo.local>
2025-02-01 16:58:42 +0100

    baremetal setup

--- applications/specfem3d_cartesian/specfem3d.py
+++ applications/specfem3d_cartesian/specfem3d.py
@@ -10,19 +10,21 @@ from reframe.core.backends import getlauncher
 class fetch_specfemd3d_cartesian(rfm.RunOnlyRegressionTest):
     descr = "Fetch SPECFEM3D_CARTESIAN repository"
 
-    repo_url = variable(str, value="https://gitlab.com/specfem_cheese_2p/full_app/specfem3d.git")
-    branch = variable(str, value="devel") 
+    repo_url = variable(str, value="https://gitlab.com/mir1995/specfem3d.git") # https://gitlab.com/specfem_cheese_2p/full_app/specfem3d.git
+    branch = variable(str, value="devel-fix-explicit-types") 
     commit = variable(str, value="c7829e0695521e104342611c212021926e87f5c2")
 
-    executable = "git clone"
+    executable = 'git clone'
     executable_opts = [
         '--recursive',
-        f'--branch {version}',
-        f'{repo_url}'
+        f'--branch {branch}',
+        f'{repo_url}',
+        'specfem3d'
     ]
+    # checkout specific commit
+    #postrun_cmds = [f'cd specfem3d && git checkout {commit}']
+    
     local = True
-
-    # git checkout specific commit
         
     @sanity_function
     def validate_download(self):
@@ -35,7 +37,6 @@ class build_specfem3d_cartesian(rfm.CompileOnlyRegressionTest):
     build_system = "Autotools"
     # build_locally = False # check first whether you can pass it from the command line
     modules = ['cuda']
-    num_gpus = parameter([1])
 
     specfem3d_cartesian = fixture(fetch_specfemd3d_cartesian, scope="test")
 
@@ -83,19 +84,13 @@ class build_specfem3d_cartesian(rfm.CompileOnlyRegressionTest):
         # I think you can remote all of the things below
         
         #self.build_system.srcdir = fullpath
-        self.build_system.options = [
-            "VERBOSE=1",
-            'LIBS="-lstdc++ -lcudart -L$NVHPC_HOME/Linux_aarch64/24.9/cuda/lib64"',
-        ]
-        self.build_system.max_concurrency = 8
-
-        gpus = int(self.num_gpus)
-        npx = int(gpus / int(gpus**0.5))
-        npy = int(gpus**0.5)
+        #self.build_system.options = [
+        #    "VERBOSE=1",
+        #    'LIBS="-lstdc++ -lcudart -L$NVHPC_HOME/Linux_aarch64/24.9/cuda/lib64"',
+        #]
+        self.build_system.make_opts = ['xmeshfem3D', 'xgenerate_databases', 'xspecfem3D']
         
-    @sanity_function
-    def validate_download(self):
-        return sn.assert_eq(self.job.exitcode, 0)
+        self.build_system.max_concurrency = 8
     
 # ========================================================
 # SPECFEM3D Base Test Class with Conditional Dependencies
@@ -109,35 +104,45 @@ class specfemd3d_base_benchmark(rfm.RunOnlyRegressionTest):
     modules = ['cuda']
     
     exclusive_access = True
-    time_limit = "600"
+    
+    num_gpus = None
 
     specfemd3d_cartesian_binaries = fixture(
         build_specfem3d_cartesian, scope="environment"
     )
+    
+    @run_after("setup")
+    def get_nproc(self):
+        # get the number of processors, ignoring comments in the Par_file
+        result = osext.run_command(f"grep -Po '^NPROC\\s*=\\s*\\K\\d+' {self.stagedir}/DATA/Par_file")
+        # Remove any whitespace and convert the output to an integer.
+        self.num_gpus = int(result.stdout.strip())
    
-    # kind = variable(str)
-    # benchmark = variable(str)
-    # metric = variable(typ.Str[r'latency|bandwidth'])
     @run_before("run")
     def setup_job_opts(self):
-        procinfo = self.current_partition.processor
-        self.num_cpus_per_task = procinfo.num_cores
-        self.num_nodes = self.specfemd3d_cartesian_binaries.num_gpus 
-        self.num_gpus_per_node = int(self.specfemd3d_cartesian_binaries.num_gpus / self.num_nodes)
+        accelerator = self.current_partition.devices
+        self.num_tasks = self.num_gpus
+        self.num_gpus_per_node =  self.num_gpus if self.num_gpus < accelerator[0].num_devices else accelerator[0].num_devices
         self.num_tasks_per_node = self.num_gpus_per_node
-        self.num_tasks = self.specfemd3d_cartesian_binaries.num_gpus
+        self.num_cpus_per_task = self.current_partition.processor.num_cpus // self.num_tasks_per_node
+        # --nodes is set automatically as job.num_tasks // num_tasks_per_node
         self.extra_resources = {
-            "nodes": {"nodes": f"{self.num_nodes}"},
+            "gpu": {"num_gpus_per_node": f"{self.num_gpus_per_node}"},
         }
+        
 
     @run_before("run")
     def set_executable_path(self):
+        self.prerun_cmds = [
+            f'mpirun -np {self.num_gpus} {os.path.join(self.specfemd3d_cartesian_binaries.build_system.sourcesdir, "bin", "xmeshfem3D")}',
+            f'mpirun -np {self.num_gpus} {os.path.join(self.specfemd3d_cartesian_binaries.build_system.sourcesdir, "bin", "xgenerate_databases")}'
+        ]
         # Set the executable path using the stagedir and build prefix
-        self.executable = 'echo hello'
-        #os.path.join(
-        #    self.specfemd3d_cartesian_binaries.specfem3d_cartesian.stagedir,
-        #    "xspecfem_mini_app",
-        #)
+        self.executable = os.path.join(
+            self.specfemd3d_cartesian_binaries.build_system.sourcesdir,
+            "bin",
+            "xspecfem3D",
+        )
 
     @run_before("run")
     def replace_launcher(self):
@@ -147,21 +152,30 @@ class specfemd3d_base_benchmark(rfm.RunOnlyRegressionTest):
     def validate_test(self):
         return sn.assert_eq(self.job.exitcode, 0)
 
-    @performance_function("s")
-    def end_time_iteration_loop(self):
-        return sn.extractsingle(
-            r"End of time iteration loop\.\.\.\s+(\S+)", self.stdout, 1, float
-        )
+    #@performance_function("s")
+    #def end_time_iteration_loop(self):
+    #    return sn.extractsingle(
+    #        r"End of time iteration loop\.\.\.\s+(\S+)", self.stdout, 1, float
+    #    )
 
-    @performance_function("count")
-    def time_step_count(self):
-        return sn.extractsingle(r"time step\s*:\s*\d+\s*/\s*(\d+)", self.stdout, 1, int)
+    #@performance_function("count")
+    #def time_step_count(self):
+    #    return sn.extractsingle(r"time step\s*:\s*\d+\s*/\s*(\d+)", self.stdout, 1, int)
 
 
 @rfm.simple_test
 class specfemd3d_iso_benchmark(specfemd3d_base_benchmark):
-    descr = "Elastic Iso [MPI + CUDA]"
-    kind = "pt2pt/standard"
-    benchmark = "osu_latency"
-    metric = "latency"
-    # executable_opts = ['-x', '3', '-i', '10']
+    descr = "specfem3d_small"
+    time_limit = "1800"
+    sourcesdir = 'loh1_256x256'
+    readonly_files = [
+        'REF_SOLUTION',
+        'DATA',
+        'readme',
+        'Check_result.py',
+        'run_preproc.sh',  
+        'run_specfem.sh'
+    ]
+    keep_files = [
+       'OUTPUT_FILES'
+    ]
